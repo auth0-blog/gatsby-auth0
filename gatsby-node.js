b0e1ade4def9028a5975b9b99091e57a43ded4c0
Sam Julien <sam.julien@auth0.com>
2019-04-11 11:46:52 -0700

    Add account component and client-only routes

---
+++ gatsby-node.js
@@ -0,0 +1,15 @@
+// ./gatsby-node.js
+// Implement the Gatsby API “onCreatePage”. This is
+// called after every page is created.
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions
+
+  // page.matchPath is a special key that's used for matching pages
+  // only on the client.
+  if (page.path.match(/^\/account/)) {
+    page.matchPath = "/account/*"
+
+    // Update the page.
+    createPage(page)
+  }
+}
---
+++ src/pages/account.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Account = () => (
+  <div>
+    <p>This is going to be a protected route.</p>
+  </div>
+)
+
+export default Account
