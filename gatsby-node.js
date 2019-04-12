2cf21d6bf00838fcf0efc644e99493474e36c4d9
Sam Julien <sam.julien@auth0.com>
2019-04-11 18:24:20 -0700

    Add Auth0 library and initial config

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,3 +13,25 @@ exports.onCreatePage = async ({ page, actions }) => {
     createPage(page)
   }
 }
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    /*
+     * During the build step, `auth0-js` will break because it relies on
+     * browser-specific APIs. Fortunately, we don’t need it during the build.
+     * Using Webpack’s null loader, we’re able to effectively ignore `auth0-js`
+     * during the build. (See `src/utils/auth.js` to see how we prevent this
+     * from breaking the app.)
+     */
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /auth0-js/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
---
+++ src/utils/auth.js
@@ -0,0 +1,13 @@
+import auth0 from "auth0-js"
+
+export const isBrowser = typeof window !== "undefined"
+
+const auth = isBrowser
+  ? new auth0.WebAuth({
+      domain: process.env.AUTH0_DOMAIN,
+      clientID: process.env.AUTH0_CLIENTID,
+      redirectUri: process.env.AUTH0_CALLBACK,
+      responseType: "token id_token",
+      scope: "openid profile email",
+    })
+  : {}
