4ef28246644b12b1796c812b2b51a20c22b11b43
Sam Julien <sam.julien@auth0.com>
2019-04-12 09:46:20 -0700

    Implement login

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,16 +1,36 @@
 import React from "react"
 import { Router } from "@reach/router"
+import { login, isAuthenticated, getProfile } from "../utils/auth"
+import { Link } from "gatsby"
 
-const Home = () => <p>Home</p>
+const Home = ({ user }) => {
+  return <p>Hi, {user.name ? user.name : "friend"}!</p>
+}
 const Settings = () => <p>Settings</p>
 const Billing = () => <p>Billing</p>
 
-const Account = () => (
-  <Router>
-    <Home path="/account/" />
-    <Settings path="/account/settings" />
-    <Billing path="/account/billing" />
-  </Router>
-)
+const Account = () => {
+  if (!isAuthenticated()) {
+    login()
+    return <p>Redirecting to login...</p>
+  }
+
+  const user = getProfile()
+
+  return (
+    <>
+      <nav>
+        <Link to="/account/">Home</Link>{" "}
+        <Link to="/account/settings/">Settings</Link>{" "}
+        <Link to="/account/billing/">Billing</Link>{" "}
+      </nav>
+      <Router>
+        <Home path="/account/" user={user} />
+        <Settings path="/account/settings" />
+        <Billing path="/account/billing" />
+      </Router>
+    </>
+  )
+}
 
 export default Account
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,5 @@
 import auth0 from "auth0-js"
+import { navigate } from "gatsby"
 
 export const isBrowser = typeof window !== "undefined"
 
@@ -11,3 +12,54 @@ const auth = isBrowser
       scope: "openid profile email",
     })
   : {}
+
+const tokens = {
+  idToken: false,
+  accessToken: false,
+  expiresAt: false,
+}
+
+let user = {}
+
+export const isAuthenticated = () => {
+  return !!localStorage.getItem("isLoggedIn")
+}
+
+export const login = () => {
+  if (!isBrowser) {
+    return
+  }
+
+  auth.authorize()
+}
+
+const setSession = (cb = () => {}) => (err, authResult) => {
+  if (err) {
+    if (err.error === "login_required") {
+      login()
+    }
+  }
+
+  if (authResult && authResult.accessToken && authResult.idToken) {
+    let expiresAt = authResult.expiresIn * 1000 + new Date().getTime()
+    tokens.idToken = authResult.idToken
+    tokens.accessToken = authResult.accessToken
+    tokens.expiresAt = expiresAt
+
+    auth.client.userInfo(tokens.accessToken, (_err, userProfile) => {
+      user = userProfile
+
+      localStorage.setItem("isLoggedIn", true)
+      navigate("/account")
+      cb()
+    })
+  }
+}
+
+export const handleAuthentication = () => {
+  auth.parseHash(setSession())
+}
+
+export const getProfile = () => {
+  return user
+}
